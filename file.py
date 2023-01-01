e015e9fa26bb677604058e8138f6d2c3d367218d
paticama <60811137+paticama@users.noreply.github.com>
2023-01-01 16:27:33 +0100

    Add files via upload

---
+++ file.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from time import strftime
+
+root = Tk()
+root.geometry("500x500")
+root.resizable(0,0)
+root.title('Python Clock')
+
+Label(root, text = 'Someting', font='arial').pack(side=BOTTOM)
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    mark.config(text = string)
+    mark.after(1000, time)
+
+mark = Label(root,font = ('calibri', 40, 'bold'),pady=150,foreground='black')
+
+mark.pack(anchor = 'center')
+time()
+
+mainloop()
